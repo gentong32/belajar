f397cf5c47f589ee274e2ea7e70ceb74f99d8383
Anto <antok9000@gmail.com>
2022-07-22 09:37:13 +0700

    commit info

---
+++ info.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>INFO</title>
+    </head>
+    <body>
+        <p>Ini adalah bagian dari informasi</p>
+    </body>
+</html>
